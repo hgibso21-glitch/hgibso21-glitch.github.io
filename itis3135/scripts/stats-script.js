3046172071c07804918a058f1223c104d91e6a12
hgibso21-glitch <hgibso21@charlotte.edu>
2026-08-31 14:10:30 -0400

    tinkering with how to get the output to show in webpage for mean output

--- itis3135/scripts/stats-script.js
+++ itis3135/scripts/stats-script.js
@@ -2,7 +2,7 @@ const getMean = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  return numbers.reduce((acc, el) => acc + el, 0) / numbers.length;
+  let mean = numbers.reduce((acc, el) => acc + el, 0) / numbers.length;
   
-  document.querySelector("#gotmean").textContent = getMean();
+  document.querySelector("#gotmean").textContent = mean;
 }
